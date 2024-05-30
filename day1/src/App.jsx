ab79a9de358d17673e32a5c07ac09540cef797f7
deepak01112002 <deepak8574757609@gmail.com>
2024-05-30 15:59:13 +0530

    first react code

---
+++ day1/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css'
+import Deepak from './Portfolio'
+
+function App() {
+  return (
+    <div>
+       
+       <Deepak/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ day1/src/Portfolio.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+
+function Deepak(){
+   return (
+     <div className='Ram'>
+         <img src="https://avatars.githubusercontent.com/u/115460382?v=4" alt="deepak" />
+        <h1>Deepak Pandey</h1>
+        <p>Passionate Full Stack Web Developer Proficient In MERN Stack With A Successful Track Record Of Building Solo And Collaborative Projects. Adaptable And Quick Learner In Generative AI. Offers Technical Expertise In React, RESTful APIs, And Git. Strong Problem-Solving And Communication Skills.</p>
+     </div>
+   )
+
+}
+
+export default Deepak

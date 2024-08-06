6a8cf85286f0149cbcb56c68ac388691a8f84a69
deepak01112002 <deepak8574757609@gmail.com>
2024-08-06 16:02:22 +0530

    CRUD Firebase

--- FireBaseCode/src/App.jsx
+++ FireBaseCode/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs } from 'firebase/firestore';
 import { db } from './Fierbase/firebase';
 
 function App() {
       const [email,setEmail] = useState("");
       const [pass,setPass] = useState("")
+      const [data,setData] = useState([])
  
         const userCollection  = collection(db,"users")
 
@@ -18,8 +19,30 @@ function App() {
           password : pass
         }
        let d = await addDoc(userCollection,obj)
+       getData()
        alert(`Data added successfully with id ${d.id}`)
       }
+     
+
+      // Getting the Data ------------------------------------------------------------ 
+     
+
+      const getData = async()=>{
+         let data = await getDocs(userCollection)
+         let arr = data.docs.map((el)=>{
+             return { id : el.id, ...el.data()}
+         })
+         setData(arr)
+      }
+      useEffect(()=>{
+         getData()
+      },[])
+    const handleDelete = async (id)=>{
+      let dell = doc(db,"users", id)
+      await deleteDoc(dell)
+      getData()
+      alert("Data Deleted Successfully")
+    }
 
   return (
     <>
@@ -28,6 +51,17 @@ function App() {
           <input type="text" onChange={(e)=>setPass(e.target.value)}/>
           <input type="submit" />
       </form>
+      {
+        data.map((el)=>{
+            return (
+                 <div key={el.id}>
+                     <h3>{el.email} - {el.password}</h3>
+                     <button onClick={()=>handleDelete(el.id)}>Delete</button>
+                     <button>Edit</button>
+                 </div>
+            )
+        })
+      }
     </>
   )
 }
